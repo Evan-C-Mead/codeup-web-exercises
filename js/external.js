e7f0abd527b561e216e19af6c361d646ecac10bf
Evan Mead <evan.c.mead@gmail.com>
2020-09-28 11:55:40 -0500

    edited text commit

--- js/external.js
+++ js/external.js
@@ -6,7 +6,6 @@ alert("Welcome to my Website!");
 
 let userColor = prompt('Please enter your favorite color: ');
 console.log('User said ' + userColor + ' is their favorite color.');
-
 alert('Great, ' + userColor + ' is my favorite color too!');
 
 alert('All movies are $3 per day');
@@ -20,23 +19,23 @@ var pricePerMovie = 3;
 var totalRentalPrice = (rentLittleMermaid * pricePerMovie) + (rentBrotherBear * pricePerMovie) + (rentHercules * pricePerMovie);
 alert('Your total price for renting is: $' + totalRentalPrice);
 
-var daysWorkedAmazon = prompt('How many days did you work at Amazon this week?');
-alert('You worked ' + daysWorkedAmazon + ' days at Amazon this week.');
+var hoursWorkedAmazon = prompt('How many hours did you work at Amazon this week?');
+alert('You worked ' + hoursWorkedAmazon + ' hours at Amazon this week.');
 var amazonPay = prompt('How much do you get paid per hour at Amazon?');
 alert('You get paid ' + amazonPay + ' per hour at Amazon.')
-var amazonTotal = daysWorkedAmazon * amazonPay;
+var amazonTotal = hoursWorkedAmazon * amazonPay;
 alert('Your total from Amazon this week is ' + amazonTotal);
-var daysWorkedFacebook = prompt('How many days did you work at Facebook this week?');
-alert('You worked ' + daysWorkedFacebook + ' days at Facebook this week.');
+var hoursWorkedFacebook = prompt('How many hours did you work at Facebook this week?');
+alert('You worked ' + hoursWorkedFacebook + ' hours at Facebook this week.');
 var facebookPay = prompt('How much do you get paid per hour at Facebook?');
 alert('You get paid ' + facebookPay + ' per hour at Facebook.')
-var facebookTotal = daysWorkedFacebook* facebookPay;
+var facebookTotal = hoursWorkedFacebook* facebookPay;
 alert('Your total from Facebook this week is ' + facebookTotal);
-var daysWorkedGoogle = prompt('How many days did you work at Google this week?');
-alert('You worked ' + daysWorkedGoogle + ' days at Google this week.');
+var hoursWorkedGoogle = prompt('How many hours did you work at Google this week?');
+alert('You worked ' + hoursWorkedGoogle + ' hours at Google this week.');
 var googlePay = prompt('How much do you get paid per hour at Google?');
 alert('You get paid ' + googlePay + ' per hour at Google.')
-var googleTotal = daysWorkedGoogle * googlePay;
+var googleTotal = hoursWorkedGoogle * googlePay;
 alert('Your total from Amazon this week is ' + googleTotal);
 var totalWeeklyPay = amazonTotal + facebookTotal + googleTotal;
 alert('Your total weekly pay is ' + totalWeeklyPay + ' .');
