947aea9600e3a6a4b3635d8be392e06c8d2658bc
Evan Mead <evan.c.mead@gmail.com>
2020-09-28 09:37:01 -0500

    function sayHello created

--- js/warmup.js
+++ js/warmup.js
@@ -9,3 +9,13 @@ console.log("Hello Ryan (both)");
 console.log("Hello Vivian");
 
 console.log("Hello Will");
+
+function sayHello(name){
+    console.log("Hello " + name);
+}
+
+sayHello("Evan");
+
+sayHello("Connor");
+
+sayHello("Rod");
