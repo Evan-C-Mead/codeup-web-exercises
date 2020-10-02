378dc1a7e5a83dc350923fd4196fdd9c3339868b
Evan Mead <evan.c.mead@gmail.com>
2020-10-02 14:15:24 -0500

    refactored FizzBuzz

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -171,9 +171,9 @@
 //  All other cases, just log the number.
 
 for (var i = 1; i <= 100; i++) {
-    if (i % 5 == 0 && i % 3 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
+    if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
     else (console.log(i));
 }
 
