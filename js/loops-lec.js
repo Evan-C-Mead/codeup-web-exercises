778ecb9d4ad2c0c3e47a9ad2b100670cce03b7f2
Evan Mead <evan.c.mead@gmail.com>
2020-10-02 14:01:39 -0500

    changed FizzBuzz

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -170,7 +170,7 @@
 //  If divisible by 3 and 5, log "FizzBuzz"
 //  All other cases, just log the number.
 
-for (var i = 1; i < 101; i++) {
+for (var i = 1; i <= 100; i++) {
     if (i % 5 == 0 && i % 3 == 0) console.log("FizzBuzz");
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
