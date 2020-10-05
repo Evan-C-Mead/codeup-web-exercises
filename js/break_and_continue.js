8fda35a92f5f806122dd0e40c7c66549f2bb2ef9
Evan Mead <evan.c.mead@gmail.com>
2020-10-05 10:32:40 -0500

    loops exercises finished

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,21 +1,21 @@
 (function (){
     "use strict";
 
-    var input;
-    while (input % 2 !== 1) {
-        input = parseFloat(prompt("Enter an odd number between 1 and 50."))
-    }
-
-    for (var i = 1; i <= 50; i++) {
-        if (input == i) {
-            console.log("Yikes! Skipping number: " + i);
-        } else {
-            if (i % 2 === 0) {
-                continue;
-            } else {
-                console.log("Here is an odd number: " + i);
-            }
-        }
-    }
+    // var input;
+    // while (input % 2 !== 1) {
+    //     input = parseFloat(prompt("Enter an odd number between 1 and 50."))
+    // }
+    //
+    // for (var i = 1; i <= 50; i++) {
+    //     if (input == i) {
+    //         console.log("Yikes! Skipping number: " + i);
+    //     } else {
+    //         if (i % 2 === 0) {
+    //             continue;
+    //         } else {
+    //             console.log("Here is an odd number: " + i);
+    //         }
+    //     }
+    // }
 
 })();
