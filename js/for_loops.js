1976dbd4b1416c28d4243206d0b61bcc640d3936
Evan Mead <evan.c.mead@gmail.com>
2020-10-05 09:20:40 -0500

    warmups for the day and fixed randomNumber for loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,32 +8,29 @@
     }
     showMultiplicationTable(7)
 
+    for (var i = 20; i <= 200; i ++) {
+        var randomNumber= Math.floor((Math.random() * 200) + 20);
+        if (randomNumber % 2 === 0) {
+            console.log(randomNumber + " is even.");
+        } else {
+            console.log(randomNumber + " is odd.")
+        }
+    }
 
-    // for (var i = 20; i <= 200; i ++) {
-    //     var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
-    //     if (randomNumber === i && i % 2 === 0) {
-    //         console.log(randomNumber + " is even.");
-    //     } else {
-    //         console.log(randomNumber + " is odd.")
+    // var i;
+    // var j;
+    // for(i=1; i<=9; i++)
+    // {
+    //     for(j=1; j<=i; j++) {
+    //         console.log(i);
     //     }
     // }
 
-
-    var i;
-    var j;
-    for(i=1; i<=9; i++)
-    {
-        for(j=1; j<=i; j++) {
-            console.log(i);
-        }
-    }
-
-
-    for (var i = 5; i <= 100; i++) {
-        if (i % 5 === 0) {
-            console.log(i);
-        }
-    }
-
+    // for (var i = 5; i <= 100; i++) {
+    //
+    //     if (i % 5 === 0) {
+    //         console.log(i);
+    //     }
+    // }
 
 })();
