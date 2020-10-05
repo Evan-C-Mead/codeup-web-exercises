a7cdf0b9297999d7d293080c0c3a118d21b0c1ca
Evan Mead <evan.c.mead@gmail.com>
2020-10-05 09:57:01 -0500

    break and continue exercise complete

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,7 +3,7 @@
 
     var input;
     while (input % 2 !== 1) {
-        input = parseFloat(prompt("Enter a number between 1 and 50."))
+        input = parseFloat(prompt("Enter an odd number between 1 and 50."))
     }
 
     for (var i = 1; i <= 50; i++) {
