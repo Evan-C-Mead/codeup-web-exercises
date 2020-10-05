5e7040f1863bfb39a81043ccc422217109e9e203
Evan Mead <evan.c.mead@gmail.com>
2020-10-05 12:26:39 -0500

    cleaned up loops exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,20 +1,29 @@
 (function (){
     "use strict";
 
-    // var input;
-    // while (input % 2 !== 1) {
-    //     input = parseFloat(prompt("Enter an odd number between 1 and 50."))
-    // }
+    // do {
+    //     var userNumber = Number(prompt("Pick an odd number between 1 and 50."));
+    //
+    //     if (userNumber < 0 || userNumber >50) {
+    //         alert("Number is not valid. Please try again.");
+    //     } else if (userNumber % 2 === 0) {
+    //         alert("Number is not odd.");
+    //     } else if (isNaN(userNumber)) {
+    //         alert("Number is not valid. Please try again");
+    //     } else {
+    //         alert("Number accepted.");
+    //         break;
+    //     }
+    // } while (true);
     //
     // for (var i = 1; i <= 50; i++) {
-    //     if (input == i) {
+    //     if (i % 2 === 0) {
+    //         continue;
+    //     }
+    //     if (i === userNumber) {
     //         console.log("Yikes! Skipping number: " + i);
     //     } else {
-    //         if (i % 2 === 0) {
-    //             continue;
-    //         } else {
-    //             console.log("Here is an odd number: " + i);
-    //         }
+    //         console.log("Here is an odd number: " + i);
     //     }
     // }
 
--- js/while.js
+++ js/while.js
@@ -9,13 +9,16 @@
     // var allCones = Math.floor(Math.random() * 50) + 50;
     //
     // do {
-    //     var totalCones = Math.floor(Math.random() * 5) + 1;
-    //     if(totalCones < allCones) {
-    //         console.log(totalCones + " cones sold...");
-    //     } else if (totalCones > allCones) {
-    //         console.log("I cannot sell you " + totalCones + " cones. I only have " + totalCones + " ...");
+    //     var conesBought = Math.floor(Math.random()* 5) + 1;
+    //
+    //     if(conesBought > allCones) {
+    //         var outOfStock = "Can't sell you " + conesBought + ". I only have " + allCones;
+    //         console.log(outOfStock);
     //     } else {
-    //         console.log("Yay! I sold them all!")
+    //         allCones -= conesBought;
+    //         var soldCones = conesBought + " cones sold..." + allCones + " cones to go";
+    //         console.log(soldCones)
     //     }
     // } while (allCones > 0);
+
 })();
