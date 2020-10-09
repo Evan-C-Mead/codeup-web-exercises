45840c3cd3ad9c6920d0e0fa882940e0796e26c7
Evan Mead <evan.c.mead@gmail.com>
2020-10-09 10:48:49 -0500

    math objects lecture methods

--- js/math-object-lec.js
+++ js/math-object-lec.js
@@ -38,7 +38,6 @@
 
     // New situation by adding a constant to random number
     // random number between 74 and 186...
-
     let numBetween = Math.ceil(Math.random() * 74) + 112;
     console.log("Using 74 as random base, and adding 112: " + numBetween)
     numBetween = Math.floor(Math.random() * 112) + 74;
@@ -47,5 +46,27 @@
     // Want a number between (decimal) 0.0000 to  0.8000
     let randomDecimal = Math.random() * 0.8;
     console.log("random decimal is: " + randomDecimal.toFixed(2));
+
+    // ==> Math pow()
+    // Raise one number to power of another number
+    let exponentResult = Math.pow(4, 2);
+    console.log("4 squared (4^2): " + exponentResult);
+    console.log("8 cubed (8^3): " + Math.pow(8, 3));
+
+    // Way to find with loop
+    let base = 8;
+    let exponent = 6;
+    let result = base;
+    for (let i = 1; i < exponent; i++) {
+        // 8, 8*8=64, 8*8*8=64*8
+        result = result * base; // when i=0, result  = 8 * 8(base)
+                                // when i=1, result = 64 * 8(base)
+                                // when i=2, result = 512 * 8(base)
+    }
+    console.log(base + " to the " + exponent + " power: " + result);
+
+    // ==> Math.sqrt()
+    console.log("The square root of 16: " + Math.sqrt(16));
+    console.log("The square root of 64: " + Math.sqrt(64));
 
 })();
