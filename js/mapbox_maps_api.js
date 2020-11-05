56b98b0193ff0f792721dc978eb7b985a501b1ca
Evan Mead <evan.c.mead@gmail.com>
2020-11-05 11:58:00 -0600

    created array of restaurant objects

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -14,38 +14,58 @@ var map = new mapboxgl.Map(mapOptions);
 
 geocode("18747 Redland Rd, San Antonio, TX 78259", mapboxToken).then(function(result) {
     console.log(result);
-    map.jumpTo(result);
+    map.flyTo(result);
     map.setZoom(10);
 
     // How to add marker with geocode result \\
     new mapboxgl.Marker()
         .setLngLat(result)
-        .setPopup(new mapboxgl.Popup().setText("La Hacienda de Los Barrios \n 18747 Redland Rd, San Antonio, TX 78259"))
+        .setPopup(new mapboxgl.Popup().setHTML("<p>La Hacienda de Los Barrios</p> <p>18747 Redland Rd, San Antonio, TX 78259</p>"))
         .addTo(map);
 });
 
 geocode("15900 La Cantera Pkwy #21200, San Antonio, TX 78256", mapboxToken).then(function(result) {
     console.log(result);
-    map.jumpTo(result);
+    map.flyTo(result);
     map.setZoom(10);
 
     // How to add marker with geocode result \\
     new mapboxgl.Marker()
         .setLngLat(result)
-        .setPopup(new mapboxgl.Popup().setText("Whiskey Cake Kitchen & Bar \n 15900 La Cantera Pkwy #21200, San Antonio, TX 78256"))
-
+        .setPopup(new mapboxgl.Popup().setHTML("<p>Whiskey Cake Kitchen & Bar</p> <p>15900 La Cantera Pkwy #21200, San Antonio, TX 78256</p>"))
         .addTo(map);
 });
 
 geocode("5450 Babcock Rd #112, San Antonio, TX 78240", mapboxToken).then(function(result) {
     console.log(result);
-    map.jumpTo(result);
+    map.flyTo(result);
     map.setZoom(10);
 
     // How to add marker with geocode result \\
     new mapboxgl.Marker()
         .setLngLat(result)
-        .setPopup(new mapboxgl.Popup().setText("Dallah Mediterranean Cuisine\n \n 5450 Babcock Rd #112, San Antonio, TX 78240"))
-
+        .setPopup(new mapboxgl.Popup().setHTML("<p>Dallah Mediterranean Cuisine</p> <p>5450 Babcock Rd #112, San Antonio, TX 78240</p>"))
         .addTo(map);
 });
+
+var restaurants = [
+    {
+        name: "La Hacienda de Los Barrios",
+        cuisineType: "Tex-Mex eatery",
+        address: "18747 Redland Rd, San Antonio, TX 78259"
+    },
+    {
+        name: "Whiskey Cake Kitchen & Bar",
+        cuisineType: "Locally sourced farm-to-kitchen dishes",
+        address: "15900 La Cantera Pkwy #21200, San Antonio, TX 78256"
+    },
+    {
+        name: "Dallah Mediterranean Cuisine",
+        cuisineType: "Mediterranean",
+        address: "5450 Babcock Rd #112, San Antonio, TX 78240"
+    }
+]
+
+restaurants.forEach(function (restaurant) {
+    console.log(restaurant)
+})
