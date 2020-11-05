189f3655b9be56d1b7ac2f5967c38117eb3d2e19
Evan Mead <evan.c.mead@gmail.com>
2020-11-05 11:21:43 -0600

    three restaurants created

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -5,8 +5,47 @@ mapboxgl.accessToken = mapboxToken;
 var mapOptions = {
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-98.4916, 29.4252], // starting position [lng, lat]
+    center: [-98.4809, 29.4949], // starting position [lng, lat]
     zoom: 10, // starting zoom
+
 }
 
 var map = new mapboxgl.Map(mapOptions);
+
+geocode("18747 Redland Rd, San Antonio, TX 78259", mapboxToken).then(function(result) {
+    console.log(result);
+    map.jumpTo(result);
+    map.setZoom(10);
+
+    // How to add marker with geocode result \\
+    new mapboxgl.Marker()
+        .setLngLat(result)
+        .setPopup(new mapboxgl.Popup().setText("La Hacienda de Los Barrios \n 18747 Redland Rd, San Antonio, TX 78259"))
+        .addTo(map);
+});
+
+geocode("15900 La Cantera Pkwy #21200, San Antonio, TX 78256", mapboxToken).then(function(result) {
+    console.log(result);
+    map.jumpTo(result);
+    map.setZoom(10);
+
+    // How to add marker with geocode result \\
+    new mapboxgl.Marker()
+        .setLngLat(result)
+        .setPopup(new mapboxgl.Popup().setText("Whiskey Cake Kitchen & Bar \n 15900 La Cantera Pkwy #21200, San Antonio, TX 78256"))
+
+        .addTo(map);
+});
+
+geocode("5450 Babcock Rd #112, San Antonio, TX 78240", mapboxToken).then(function(result) {
+    console.log(result);
+    map.jumpTo(result);
+    map.setZoom(10);
+
+    // How to add marker with geocode result \\
+    new mapboxgl.Marker()
+        .setLngLat(result)
+        .setPopup(new mapboxgl.Popup().setText("Dallah Mediterranean Cuisine\n \n 5450 Babcock Rd #112, San Antonio, TX 78240"))
+
+        .addTo(map);
+});
